62b82285e62fc42ee36534e162bcf239df4471a8
khanhd5320 <khanhd5320@gmail.com>
2019-10-28 01:20:43 +0700

    Commit Cuối cùng trước ngày báo cáo chỉnh lại màu của các nav và nav avatar

--- QuanLyCuaHang/app/src/main/java/com/example/quanlycuahang/Admin/ThongKeFragment.java
+++ QuanLyCuaHang/app/src/main/java/com/example/quanlycuahang/Admin/ThongKeFragment.java
@@ -5,18 +5,27 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.quanlycuahang.Admin.HoaDon.HoaDon;
+import com.example.quanlycuahang.Admin.HoaDon.HoaDonFirebaseHelper;
 import com.example.quanlycuahang.R;
+
+import java.util.Date;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ThongKeFragment extends Fragment {
-
+    View vRoot;
+    private Button btnTheongay;
 
     public ThongKeFragment() {
         // Required empty public constructor
@@ -26,8 +35,16 @@ public class ThongKeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        vRoot = inflater.inflate(R.layout.fragment_thong_ke, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_thong_ke, container, false);
+        init();
+        return vRoot;
+    }
+
+    private void init() {
+        btnTheongay = vRoot.findViewById(R.id.btnTheongay);
+
+
     }
 
 }
